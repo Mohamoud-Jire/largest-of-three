7777a1017b3c34c372ec93bc18c44b82d08d0c92
Mohamoud Jire <119512193+Mohamoud-Jire@users.noreply.github.com>
2023-07-13 17:13:28 +0300

    Add files via upload

---
+++ largest-of-three/challenge.js
@@ -0,0 +1,13 @@
+// Good Luck! You got this 💪🏾
+// Write your code here.
+let num1 = 100;
+let num2 = 200;
+let num3 = 300;
+
+if(num1 > num2 && num1 > num2) {
+  console.log(`The largest number out of the three is ${num1}`);
+} else if(num2 > num1 && num2 > num3) {
+  console.log(`The largest number out of the three is ${num2}`);
+} else if(num3 > num1 && num3 > num2) {
+  console.log(`The largest number out of the three is ${num3}`);
+}
